85c28888a65f822b35c97e8236517c2f9ffaa3bb
Jatin Mourya <jatinmourya5523@gmail.com>
2026-09-18 21:15:13 +0530

    Fix code

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,11 +4,6 @@ const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
-// module.exports.index = async (req, res) => {
-//     const allListings = await Listing.find({});
-
-//     res.render("listings/index.ejs", { allListings });
-// };
 
 
 
@@ -43,32 +38,9 @@ module.exports.showListing = async (req, res) => {
     req.flash("error", "Listing you requested for does't exist!");
    return res.redirect("/listings");
   }
-  // console.log(listing);
   res.render("listings/show.ejs", { listing });
 };
 
-// module.exports.createListing = async (req, res) => {
-//   let response = await geocodingClient.forwardGeocode({
-//   query: req.body.listing.location,
-//   limit: 1,
-// })
-//   .send();
-
-
-//   let url = req.file.path;
-//   let filename = req.file.filename;
-  
-//     const newListing = new Listing(req.body.listing);
-//     newListing.owner = req.user._id;
-//     newListing.image = { url, filename };
-
-//     newListing.geometry = response.body.features[0].geometry;
-
-//     let savedListing = await newListing.save();
-//     console.log(savedListing);
-//     req.flash("success", "New Listing Created!");
-//     res.redirect("/listings");
-// };
 
 module.exports.createListing = async (req, res) => {
   let url = req.file.path;
